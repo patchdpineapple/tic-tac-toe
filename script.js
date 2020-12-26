b0c305fee078b86c2c0ed3242e8e437c33fcd7cd
patchdpineapple <seanpatrick.penaranda@gmail.com>
2020-12-26 18:11:35 +0800

    added start form and clickable board

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //
-const gameinitialize = (function (){
+const gamemaster = (function () {
 
 
 
@@ -14,20 +14,87 @@ const gameinitialize = (function (){
 
 
 // Game module
-const game = (function (){
+const game = (function () {
 
-    let gameboard = ['O','X','X','O','X','O','X','O','X'];
-    
+    let _turn = 1;
 
-    function initializeboard(){
-        
+
+    let gameboard = {
+        boardArray: ['O', 'X', 'X', 'O', 'X', 'O', 'X', 'O', 'X'],
+
+        fillBoard: function () {
+            for (let i = 0; i < this.boardArray.length; i++) {
+                document.getElementById(`${i}`).textContent = this.boardArray[i];
+
+            }
+        },
+
+        resetBoard: function () {
+            for (let i = 0; i < this.boardArray.length; i++) {
+                document.getElementById(`${i}`).textContent = '';
+
+            }
+        },
+
+
+
+    };
+
+    function _checkWinner() {
+
 
     }
+
+    function _displayWinner() {
+
+    }
+
+    function _setBoard() {
+        for (let i = 0; i < gameboard.boardArray.length; i++) {
+            document.getElementById(`${i}`).addEventListener('click', (e) => {
+                let target = e.target;
+                if(_turn == 1){
+                    target.textContent = 'O';
+                    _turn = 2;
+                }
+                else if(_turn == 2){
+                    target.textContent = 'X';
+                    _turn = 1;
+                }
+
+
+            });
+        }
+
+    }
+
+    
+
+
+
+    function start() {
+        _setBoard();
+
+        let name1 = document.getElementById('formPlayer1Name').value;
+        let name2 = document.getElementById('formPlayer2Name').value;
+        document.getElementById('player1name').textContent = name1;
+        document.getElementById('player2name').textContent = name2;
+        document.getElementById('startpage').style.display = "none";
+
+    }
+
+    function resetForm() {
+        document.getElementById('formPlayer1Name').value = '';
+        document.getElementById('formPlayer2Name').value = '';
+    }
+
 
 
 
     return {
         gameboard,
+        start,
+        resetForm,
     };
 
 
@@ -36,17 +103,19 @@ const game = (function (){
 
 
 //Player factory
-const Player = function (name) {
+const Player = function (name, mark) {
 
     let playername = '';
 
-    function setName(name){
+    function setName(name) {
         playername = name;
     }
 
-    function addMark(){
-
+    function addMark() {
+        game.gameboard.markboard('O');
     }
 
-    return {playername, addMark};
+    return { playername, addMark };
 }
+
+game.resetForm();
